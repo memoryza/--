5940773006efb9acb38b40bc467bce3dafd4d4ee
王金财 <jincai.wang@foxmail.com>
2015-03-03 15:37:53 +0800

    add getAllkeys

--- js/cache.js
+++ js/cache.js
@@ -29,16 +29,17 @@
 	    	return false;
 	    }
 	}
-	exports.set = function(key, val) {};
-	exports.get = function(key, defaultVal) {};
-	exports.del = function(key) {};
-	exports.clear = function() {};
-	exports.some =  function(match) {};
-	exports.getItems = function(keys) {};
- 	exports.serialize = function(val) {
+	exports.set = function (key, val) {};
+	exports.get = function (key, defaultVal) {};
+	exports.remove = function (key) {};
+	exports.clear = function () {};
+	exports.some =  function (match) {};
+	exports.getItems = function (keys) {};
+	exports.getAllkeys =  function () {};
+ 	exports.serialize = function (val) {
  		return JSON.stringify( val );
  	}
- 	exports.deserialize = function(val) {
+ 	exports.deserialize = function (val) {
  		if (typeof val != 'string') { 
  			return undefined;
  		}
@@ -50,26 +51,26 @@
  	}
  	exports.supported = isLocalStorageNameSupported();
  	if(exports.supported) {
- 		exports.set = function(key, val) {
+ 		exports.set = function (key, val) {
 			if ( val === undefined ) { 
 				return store.remove(key);
 			}
 			storage.setItem(key, exports.serialize(val));
 			return val;
 	 	}
-	 	exports.get = function(key, defaultVal) {
+	 	exports.get = function (key, defaultVal) {
 	 		var val = exports.deserialize(storage.getItem(key));
 			return (val === undefined ? defaultVal : val);
 	 	}
-	 	exports.remove = function(key) {
+	 	exports.remove = function (key) {
 	 		var val = exports.get(key);
 	 		storage.removeItem(key);
 	 		return val;
 	 	}
-	 	exports.clear = function() {
+	 	exports.clear = function () {
 	 		storage.clear();
 	 	}
-	 	exports.some = function(match) {
+	 	exports.some = function (match) {
 	 		var _len = storage.length;
 	 		var key = '';
 	 		var pattern = new RegExp(match);
@@ -82,12 +83,19 @@
 	 		}
 	 		return keys;
 	 	}
-	 	exports.getItems = function(keys) {
+	 	exports.getItems = function (keys) {
 	 		var itemList = {};
 	 		for(var i = 0, _len = keys.length; i < _len; i++) {
 	 			itemList[keys[i]] = exports.get(keys[i]);
 	 		}
 	 		return itemList;
+	 	}
+	 	exports.getAllkeys = function () {
+	 		var keys = [];
+	 		for (var i = 0, _len = storage.length; i < _len; i++) {
+	 			keys.push(storage.key(i));
+	 		}
+	 		return keys;
 	 	}
  	}
  	return exports;
